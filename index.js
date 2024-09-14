d89029d75cfc26f59efedba17e27258f3917e18a
Jhoan <jhoans.8484@gmail.com>
2024-09-14 08:12:32 -0500

    index /database

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ app
 // Ruta principal
 app.get('/', (req, res) => res.render('pages/index'));
 
-// Ruta de prueba
-app.get('/prueba', (req, res) => {
-  console.log('correcto');
-  res.send('Prueba correcta');
+// database
+app.get('/database', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM personas');
+    res.render('pages/index', { personas: result.rows });
+  } catch (err) {
+    console.error(err);
+    res.send('Error al conectar a la base de datos.');
+  }
 });
-
-// Ruta html - jes
-app.get('/jes', (req, res) => res.render('pages/prueba'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
