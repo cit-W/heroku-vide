58a736946b6446db6e973aff3dc0ced2e2fd9c17
Jhoan <jhoans.8484@gmail.com>
2024-09-21 12:43:22 -0500

    correccion

--- routes/general.js
+++ routes/general.js
@@ -67,21 +67,26 @@ router.post('/delete_reservas', async (req, res) => {
 
 // asistencia---------------------------------------------------------------
 
-// GET - Obtener registros por ID del estudiante
-router.get('/', async (req, res) => {
+// POST - Registrar asistencia
+router.post('/marcar', async (req, res) => {
+  const { id, fecha } = req.query;
+
+  if (!id || !fecha) {
+    return res.status(400).json({ success: false, error: "Faltan datos para completar el registro" });
+  }
 
   try {
-    const query = 'SELECT * FROM asistencia.asistencia_diaria';
-    const result = await pool.query(query);
-
-    if (result.rows.length > 0) {
-      res.json({ success: true, data: result.rows });
-    } else {
-      res.status(404).json({ success: false, message: "No se encontraron info para el id proporcionado" });
-    }
+    const query = `
+      INSERT INTO asistencia.asistencia_diaria 
+      (id, fecha) 
+      VALUES ($1, $2)
+    `;
+    const values = [id, fecha];
+    await pool.query(query, values);
+    res.json({ success: true, message: "Asistencia registrada con éxito" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, error: "Error al registrar el reporte" });
   }
 });
 
@@ -105,29 +110,6 @@ router.get('/registro_diario', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ success: false, error: err.message });
-  }
-});
-
-// POST - Registrar asistencia
-router.post('/marcar', async (req, res) => {
-  const { id, fecha } = req.query;
-
-  if (!id || !fecha) {
-    return res.status(400).json({ success: false, error: "Faltan datos para completar el registro" });
-  }
-
-  try {
-    const query = `
-      INSERT INTO asistencia.asistencia_diaria 
-      (id, fecha) 
-      VALUES ($1, $2)
-    `;
-    const values = [id, fecha];
-    await pool.query(query, values);
-    res.json({ success: true, message: "Asistencia registrada con éxito" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: "Error al registrar el reporte" });
   }
 });
 
