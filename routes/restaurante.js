43a4891c79bfbb91a156581d9a1f15e7d346fcd9
Jhoan <jhoans.8484@gmail.com>
2024-09-24 08:58:46 -0500

    correcciones / de restaurante

--- routes/restaurante.js
+++ routes/restaurante.js
@@ -26,46 +26,45 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM restaurante.lista_general');
-      const personas = result.rows;
-      client.release();
-      
-      if (result.rows.length > 0) {
-            res.json(result.rows);
-        } else {
-            res.send("No_hay_tablas");
-        }
-
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM restaurante.lista_general');
+    const personas = result.rows;
+    client.release();
+    
+    if (result.rows.length > 0) {
+      res.json(result.rows);
+    } else {
+      res.send("No_hay_tablas");
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 // Ruta para subir el archivo .xlsx
 router.post('/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).send('No se subió ningún archivo.');
   }
-
+  
   try {
     // Leer el archivo .xlsx
     const workbook = xlsx.readFile(req.file.path);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     const data = xlsx.utils.sheet_to_json(worksheet);
-
+    
     // Insertar datos en la base de datos
     const client = await pool.connect();
     const insertQuery = 'INSERT INTO restaurante.lista_general(nombre, id, curso, pago_mensual) VALUES($1, $2, $3, $4)';
-
+    
     for (let row of data) {
       const { nombre, id, curso, pago_mensual } = row;
       await client.query(insertQuery, [nombre, id, curso, pago_mensual]);
     }
-
+    
     client.release();
     res.send('Datos insertados correctamente en lista_general');
   } catch (error) {
@@ -75,12 +74,12 @@ router.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 router.post('/update-pago', async (req, res) => {
-  const { id, pago_mensual } = req.query;
-
+  const { id, pago_mensual } = req.query;  // Cambiado de req.query a req.body
+  
   if (!id || typeof pago_mensual === 'undefined') {
     return res.status(400).send('ID y pago_mensual son requeridos.');
   }
-
+  
   try {
     const client = await pool.connect();
     const updateQuery = 'UPDATE restaurante.lista_general SET pago_mensual = $1 WHERE id = $2';
